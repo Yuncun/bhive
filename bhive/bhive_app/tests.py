ff8bf6db2ef1abd96bb7ee8801786c564a7aeb12
Matteo Simone <matteo.j.simone@gmail.com>
2014-09-09 08:52:19 -0700

    Fixed voting api

--- bhive/bhive_app/tests.py
+++ bhive/bhive_app/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory
+
+from bhive_app.models import Answer, Question
+from bhive_app.views import AnswerVote
+
+
+class VotingViewTestCase(TestCase):
+    factory = APIRequestFactory()
+
+    def setUp(self):
+        super(VotingViewTestCase, self).setUp()
+        self.question = Question.objects.create(text='?', user_id='1')
+        self.answer = self.question.answer_set.create(text='A')
+
+    def test_data_missing(self):
+        request = self.factory.put('/answers/1/', data={})
+        view = AnswerVote.as_view()
+        response = view(request, pk=self.answer.pk).render()
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_put_false(self):
+        request = self.factory.put('/answers/1/', data={'vote': False})
+        view = AnswerVote.as_view()
+        response = view(request, pk=self.answer.pk).render()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        content = json.loads(response.content)
+        self.assertEqual(content['votes'], 0)
+        answer = Answer.objects.get(pk=self.answer.pk)
+        self.assertEqual(answer.votes, 0)
+
+    def test_put(self):
+        request = self.factory.put('/answers/1/', data={'vote': True})
+        view = AnswerVote.as_view()
+        response = view(request, pk=self.answer.pk).render()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        content = json.loads(response.content)
+        self.assertEqual(content['votes'], 1)
+        answer = Answer.objects.get(pk=self.answer.pk)
+        self.assertEqual(answer.votes, 1)
--- bhive/bhive_app/views.py
+++ bhive/bhive_app/views.py
@@ -1,7 +1,13 @@
+from django.db.models import F
+from rest_framework import status
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, UpdateAPIView
+from rest_framework.response import Response
 
 from bhive_app.models import Answer, Question
 from bhive_app.serializers import AnswerSerializer, QuestionSerializer
+
+
+TRUE_VALUES = ['True', 'true', 1]
 
 
 class QuestionsList(ListCreateAPIView):
@@ -19,7 +25,16 @@ class AnswerVote(UpdateAPIView):
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
 
-    def pre_save(self, obj):
-        vote = self.request.DATA.get('vote')
-        if vote:
-            obj.votes += 1
+    def update(self, request, *args, **kwargs):
+        vote = self.request.DATA.get('vote', None)
+        obj = self.get_object_or_none()
+
+        if vote is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        if vote in TRUE_VALUES:
+            Answer.objects.filter(id__in=[obj.pk]).update(votes=F('votes') + 1)
+
+        obj = self.get_object_or_none()
+        serializer = self.get_serializer(obj)
+        return Response(serializer.data, status=status.HTTP_200_OK)
