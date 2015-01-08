1feace534d8fa8e55a65eb26ecf1f4565ec87c80
Yuncun Shen <yuncun@ucla.edu>
2015-01-08 02:35:14 -0800

    Attempting to add image field

--- bhive/settings/base.py
+++ bhive/settings/base.py
@@ -33,6 +33,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	
+	#S3 storage for imagefields
+	'storages',
 
     # third party apps
     'rest_framework',
@@ -40,6 +43,25 @@ INSTALLED_APPS = (
     # hive internal apps
     'bhive_app',
 )
+
+#AWS Credentials
+AWS_STORAGE_BUCKET_NAME = 'hiveapp'
+AWS_ACCESS_KEY_ID = 'AKIAIITQSQZBSBGUMVCQ'
+AWS_SECRET_ACCESS_KEY = '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'
+
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# This is used by the `static` template tag from `static`, if you're using that. Or if anything else
+# refers directly to STATIC_URL. So it's safest to always set it.
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
