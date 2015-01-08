8004efcdcca455bf5393a6f3a105e90272d90d9e
Yuncun Shen <yuncun@ucla.edu>
2015-01-08 14:37:15 -0800

    Reverted changes

--- bhive_app/models.py
+++ bhive_app/models.py
@@ -11,17 +11,17 @@ class Question(models.Model):
         return u'Question #{}'.format(self.pk)
 
 
-def upload_pic_to(instance, filename):
-    import os
-    from django.utils.timezone import now
-    filename_base, filename_ext = os.path.splitext(filename)
-    return 'pictures/%s%s' % (
-        now().strftime("%Y%m%d%H%M%S"),
-        filename_ext.lower(),
-    )
+# def upload_pic_to(instance, filename):
+    # import os
+    # from django.utils.timezone import now
+    # filename_base, filename_ext = os.path.splitext(filename)
+    # return 'pictures/%s%s' % (
+        # now().strftime("%Y%m%d%H%M%S"),
+        # filename_ext.lower(),
+    # )
 
 class Answer(models.Model):
-    picture = models.ImageField(("Picture"), upload_to=upload_pic_to, blank=True)
+   # picture = models.ImageField(("Picture"), upload_to=upload_pic_to, blank=True)
     question = models.ForeignKey(Question)
     text = models.CharField(max_length=25)
     votes = models.IntegerField(default=0)
